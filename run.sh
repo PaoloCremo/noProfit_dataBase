993d92685be75a7197b2a6da4497ad49d2449fc7
Paolo Cremonese <cremonesep25@gmail.com>
2025-01-27 15:16:27 -0800

    file to run the pipeline

---
+++ run.sh
@@ -0,0 +1,4 @@
+link="https://www.amcnposolutions.com/directory-of-canadian-not-for-profit-associations/bc-a-to-c/"
+database="/path/to/database"
+n_a=50
+python3 automation.py -n $n_a -l $n_a -s $link -p $database -v true
